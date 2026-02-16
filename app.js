a3e72a9beb7c56f9c5bf957be7d0d3b1780ba6d1
Cohen <Marc.Cohen@sanofi.com>
2026-02-16 15:42:55 -0500

    Set Apps Script web app URL

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_WEB_APP_URL = "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/u/0/home/projects/1yRCzOHp_zjzGTGP9NYC_5bNxO2CiHdrap1l3L16DFgvoYL2w63f4nGCu/edit";
 
 const roastReplies = [
   "Get the hell out of here with that chaos-tier suggestion.",
